d931f8e3cad3ec717acfe507a72b7541197a27ac
CESAR TZOC <csartalva01@gmail.com>
2022-04-13 23:01:36 -0600

    project creation in spring and angular

---
+++ Maya-Academy/src/main/java/com/maya/academy/MayaAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.maya.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayaAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayaAcademyApplication.class, args);
+	}
+
+}
---
+++ Maya-Academy/src/test/java/com/maya/academy/MayaAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maya.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayaAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
