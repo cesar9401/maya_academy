0e216ae61def8c3ec6eb0dfb6906a27b8aba7f21
CESAR TZOC <csartalva01@gmail.com>
2022-04-15 15:35:28 -0600

    changed toolbar of quilljs

--- maya-academy-frontend/src/app/editor/editor.component.ts
+++ maya-academy-frontend/src/app/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
 
 @Component({
 	selector: 'app-editor',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./editor.component.css'],
 })
 export class EditorComponent implements OnInit {
-	code: any;
+	modules: {};
+	editorText: String;
+	output: HTMLElement;
 
 	constructor() {}
 
 	ngOnInit(): void {
+		this.output = document.querySelector(".output")
+
+		this.modules = {
+			toolbar: [
+				['bold', 'italic', 'underline', 'strike'], // toggled buttons
+				['code-block'],
+
+				[{ header: 1 }, { header: 2 }], // custom button values
+				[{ list: 'ordered' }, { list: 'bullet' }],
+				[{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+				[{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+
+				[{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+				[{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+				[{ color: [] }], // dropdown with defaults from theme
+				[{ font: [] }],
+				[{ align: [] }],
+
+				['link', 'image', 'video'], // link and image, video
+			],
+		};
+	}
+
+	editorChanged(event: EditorChangeContent | EditorChangeSelection) {
+		this.editorText = event.editor.root.innerHTML;
 	}
 }
