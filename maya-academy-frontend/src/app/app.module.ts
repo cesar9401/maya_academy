8bdc224b8b9d686dffd1d42656746570d7d84858
froi0409 <ferrubocana@gmail.com>
2022-04-19 15:42:18 -0600

    Se han agregado los componentes para la página principal, inicio de sesión y registro de usuario

--- maya-academy-frontend/src/app/app.module.ts
+++ maya-academy-frontend/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { HomepageContentComponent } from './homepage-content/homepage-content.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [AppComponent, ToolBarComponent, HomepageContentComponent, LoginComponent, RegisterComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
