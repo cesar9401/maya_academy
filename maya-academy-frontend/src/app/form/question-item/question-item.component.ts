8bbdc269e2f0225bca5b4e2e23ba2240eab97855
CESAR TZOC <csartalva01@gmail.com>
2022-04-21 02:21:28 -0600

    rendering bootstrap's cards for questions

--- maya-academy-frontend/src/app/form/question-item/question-item.component.ts
+++ maya-academy-frontend/src/app/form/question-item/question-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Option } from 'src/app/model/option.model';
 import { Question } from 'src/app/model/question.model';
 
@@ -8,57 +8,36 @@ import { Question } from 'src/app/model/question.model';
 	styleUrls: ['./question-item.component.css'],
 })
 export class QuestionItemComponent implements OnInit {
+	classes = [
+		'border-primary',
+		'border-secondary',
+		'border-danger',
+		'border-warning',
+		'border-info',
+	];
 	@Input() question: Question;
 	@Input() index: number;
-	// @Output() close = new EventEmitter();
-	options: Option[];
+	@Output() questionIndex = new EventEmitter<{
+		index: number;
+		data: string;
+	}>();
 
-	constructor() {}
+	constructor() {
+	}
 
 	ngOnInit(): void {
-		// this.question = new Question();
-		this.question.questionTitle = `Pregunta ${this.index + 1}`;
-		const opt1 = new Option();
-		opt1.correct = false;
-		opt1.optionTitle = "Opcion 1";
-
-		const opt2 = new Option();
-		opt2.optionTitle = 'Opcion B';
-		opt2.correct = true;
-
-		this.options = [opt1, opt2];
-		this.question.options = this.options;
-
-		// this.question = new Question();
-		// this.question.questionTitle = 'Titulo de la pregunta 1';
-		// this.question.description = 'description';
-		// this.question.score = 10;
-
-		// const opt1 = new Option();
-		// opt1.optionTitle = 'Opcion A';
-		// const opt2 = new Option();
-		// opt2.optionTitle = 'Opcion B';
-		// const opt3 = new Option();
-		// opt3.optionTitle = 'Opcion C';
-		// this.options = [opt1, opt2, opt3];
-
-		// this.question.options = this.options;
+		console.log(this.classes[this.index % this.classes.length])
 	}
 
 	addOption() {
 		const option = new Option();
-		option.optionTitle = `Opcion ${this.options.length + 1}`
+		option.optionTitle = `Opcion ${this.question.options.length + 1}`;
 		option.correct = false;
-		this.options.push(option);
+		this.question.options.push(option);
 	}
 
 	deleteOption(i: number) {
-		this.options.splice(i, 1);
-	}
-
-	ver() {
-		console.log(this.question);
-		console.log(this.checkQuestion());
+		this.question.options.splice(i, 1);
 	}
 
 	changeQuestionTitle(event) {
@@ -70,38 +49,20 @@ export class QuestionItemComponent implements OnInit {
 	}
 
 	changeInput(event, i: number) {
-		this.options[i].optionTitle = event.target.value;
+		this.question.options[i].optionTitle = event.target.value;
 	}
 
 	changeRadio(event, i) {
-		(event.target.value, i);
-		this.options.forEach((option, index) => {
+		event.target.value, i;
+		this.question.options.forEach((option, index) => {
 			option.correct = false;
-			if(index === i) {
+			if (index === i) {
 				option.correct = true;
 			}
-		})
+		});
 	}
 
-	checkQuestion(): boolean {
-		if(!this.question.questionTitle || !this.question.questionTitle.trim()) {
-			return false;
-		}
-
-		if(!this.question.options.length) {
-			return false;
-		}
-
-		const anyCorrect = this.options.every(option => !option.correct);
-		if(anyCorrect) {
-			return false;
-		}
-
-		const anyWithoutTitle = this.options.some(option => option.optionTitle.trim() === '');
-		if(anyWithoutTitle) {
-			return false;
-		}
-
-		return true;
+	emitQuestionIndex(data: string) {
+		this.questionIndex.emit({ index: this.index, data: data });
 	}
 }
--- maya-academy-frontend/src/app/form/question-list/question-list.component.ts
+++ maya-academy-frontend/src/app/form/question-list/question-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Option } from 'src/app/model/option.model';
 import { Question } from 'src/app/model/question.model';
 
 @Component({
@@ -12,10 +13,72 @@ export class QuestionListComponent implements OnInit {
 	constructor() {}
 
 	ngOnInit(): void {
-		this.questions = [new Question(), new Question(), new Question()];
+		this.questions = [this.addQuestion(), this.addQuestion()];
 	}
 
-	see() {
+	createQuestions() {
+		const correct = this.questions.every(q => this.checkQuestion(q));
+		console.log(correct);
+	}
+
+	receiveQuestionIndex(obj: { index: number; data: string }) {
+		switch (obj.data) {
+			case 'close':
+				this.questions.splice(obj.index, 1);
+				break;
+			case 'up':
+				this.insertElement(obj.index, this.addQuestion());
+				break;
+			case 'down':
+				this.insertElement(obj.index + 1, this.addQuestion());
+				break;
+		}
+
 		console.log(this.questions);
+	}
+
+	addQuestion(): Question {
+		const question = new Question();
+		question.questionTitle = 'Pregunta';
+		question.options = [];
+
+		return question;
+	}
+
+	insertElement(index: number, item: Question) {
+		this.questions = [
+			...this.questions.slice(0, index),
+			item,
+			...this.questions.slice(index),
+		];
+	}
+
+	checkQuestion(question: Question): boolean {
+		if (
+			!question.questionTitle ||
+			!question.questionTitle.trim()
+		) {
+			return false;
+		}
+
+		if (!question.options.length) {
+			return false;
+		}
+
+		const anyCorrect = question.options.every(
+			(option) => !option.correct
+		);
+		if (anyCorrect) {
+			return false;
+		}
+
+		const anyWithoutTitle = question.options.some(
+			(option) => option.optionTitle.trim() === ''
+		);
+		if (anyWithoutTitle) {
+			return false;
+		}
+
+		return true;
 	}
 }
