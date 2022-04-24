2f735f5cfcea4329061c240a8cc6456527ff366f
potzmevi <potz@DESKTOP-GN7Q675.localdomain>
2022-04-23 21:18:53 -0600

    [JMeza] created view Component form

--- maya-academy-frontend/src/app/form/question-item-view/question-item-view.component.ts
+++ maya-academy-frontend/src/app/form/question-item-view/question-item-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { Option } from 'src/app/model/option.model';
+import { Question } from 'src/app/model/question.model';
 
 @Component({
   selector: 'app-question-item-view',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-item-view.component.css']
 })
 export class QuestionItemViewComponent implements OnInit {
-
+  @Input() question: Question;
+	@Input() index: number;
   constructor() { }
-
+	options: Option[];
   ngOnInit(): void {
+    this.question.questionTitle = `Pregunta ${this.index + 1}`;
+		const opt1 = new Option();
+		opt1.correct = false;
+		opt1.optionTitle = "Opcion 1";
+
+		const opt2 = new Option();
+		opt2.optionTitle = 'Opcion B';
+		opt2.correct = true;
+
+		this.options = [opt1, opt2];
+		this.question.options = this.options;
   }
 
+  
 }
--- maya-academy-frontend/src/app/form/question-list-view/question-list-view.component.ts
+++ maya-academy-frontend/src/app/form/question-list-view/question-list-view.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Question } from 'src/app/model/question.model';
 @Component({
   selector: 'app-question-list-view',
   templateUrl: './question-list-view.component.html',
   styleUrls: ['./question-list-view.component.css']
 })
 export class QuestionListViewComponent implements OnInit {
+  questions: Question[];
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+		this.questions = [new Question(), new Question(), new Question()];
+	}
 
 }
